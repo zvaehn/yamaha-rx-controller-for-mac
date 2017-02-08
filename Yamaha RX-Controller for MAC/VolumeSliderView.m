216c4d8943d3db876f77b98e2a943121ab1132ef
Sven Schiffer <svenschiffer92@gmail.com>
2017-02-08 21:49:24 +0100

    added statusicon

---
+++ Yamaha RX-Controller for MAC/VolumeSliderView.m
@@ -0,0 +1,19 @@
+//
+//  VolumeSliderView.m
+//  Yamaha RX-Controller for MAC
+//
+//  Created by Sven Schiffer on 08/02/2017.
+//  Copyright © 2017 Sven Schiffer. All rights reserved.
+//
+
+#import "VolumeSliderView.h"
+
+@implementation VolumeSliderView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
