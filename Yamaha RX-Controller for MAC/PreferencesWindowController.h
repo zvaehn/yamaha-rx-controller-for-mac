9b3a8bfdfee4edad43f65359c532a03c26705614
Sven Schiffer <svenschiffer92@gmail.com>
2017-02-11 13:34:19 +0100

    renamed screenshot, added prefWindowController

---
+++ Yamaha RX-Controller for MAC/PreferencesWindowController.h
@@ -0,0 +1,13 @@
+//
+//  PreferencesWindowController.h
+//  Yamaha RX-Controller for MAC
+//
+//  Created by Sven Schiffer on 10/02/2017.
+//  Copyright © 2017 Sven Schiffer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PreferencesWindowController : NSWindowController
+
+@end
---
+++ Yamaha RX-Controller for MAC/PreferencesWindowController.m
@@ -0,0 +1,23 @@
+//
+//  PreferencesWindowController.m
+//  Yamaha RX-Controller for MAC
+//
+//  Created by Sven Schiffer on 10/02/2017.
+//  Copyright © 2017 Sven Schiffer. All rights reserved.
+//
+
+#import "PreferencesWindowController.h"
+
+@interface PreferencesWindowController ()
+
+@end
+
+@implementation PreferencesWindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
